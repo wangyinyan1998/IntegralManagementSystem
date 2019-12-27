f897be719f9f3f9f0ed0165894f972c12881e1da
Yanshiyi <shiyi.yan@sap.com>
2019-12-27 21:35:42 +0800

    增加 test

---
+++ IntegralManagementSystem/src/test/ControllerTest.java
@@ -0,0 +1,38 @@
+package test;
+
+import static org.junit.Assert.*;
+
+public class ControllerTest {
+
+    @org.junit.Test
+    public void addUser() {
+    }
+
+    @org.junit.Test
+    public void addTask() {
+    }
+
+    @org.junit.Test
+    public void doTask() {
+    }
+
+    @org.junit.Test
+    public void consume() {
+    }
+
+    @org.junit.Test
+    public void newItem() {
+    }
+
+    @org.junit.Test
+    public void checkFlow() {
+    }
+
+    @org.junit.Test
+    public void checkAccount() {
+    }
+
+    @org.junit.Test
+    public void findCanDoTask() {
+    }
+}
