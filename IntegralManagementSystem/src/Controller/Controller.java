7c3bf172bf9ecbdf295a7a2fb353cab0aa99fc25
wyy <16302010074@fudan.edu.cn>
2019-12-27 21:24:55 +0800

    上传第二次代码

--- IntegralManagementSystem/src/Controller/Controller.java
+++ IntegralManagementSystem/src/Controller/Controller.java
@@ -1,11 +1,12 @@
 package Controller;
 
 import item.Item;
+import transaction.Account;
 import transaction.ConsumerTransaction;
 import transaction.Flow;
+import transaction.PerformTaskTransaction;
 import user.TaskPerformer;
-import userTask.Task;
-import userTask.UserTask;
+import userTask.*;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -13,7 +14,6 @@ import java.io.ObjectInputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.concurrent.ExecutionException;
 
 public class Controller {
     static ArrayList<Task> taskArrayList=new ArrayList<>();
@@ -35,12 +35,16 @@ public class Controller {
             String instr = input.next();
             switch(instr){
                 case "u":
+                    addUser(input.next());
                     break;
                 case "t":
+                    addTask(input);
                     break;
                 case "doTask":
+                    doTask(input.next(), input.next());
                     break;
                 case "fd":
+                    findCanDoTask(input.next());
                     break;
                 case "account":
                     checkAccount(input.next());
@@ -55,6 +59,168 @@ public class Controller {
                     consume(input.next(),input.next());
                     break;
             }
+        }
+    }
+    public static void addUser(String username) {
+        Account account = new Account();
+        taskPerformers.add(new TaskPerformer(username, account));
+    }
+
+    public static void addTask(Scanner input) {
+        System.out.println("请输入要创建的task的类型，如果是一次性task，请输入 once taskname point" +
+                "如果是可重复Task，请输入 repeat1 taskname point lifeCycle" +
+                "如果是每日可重复Task，请输入 repeat2 taskname point lifeCycle");
+        String instr = input.next();
+        String taskName;
+        int point;
+        int count;
+        TaskDef taskDef;
+        FixPointStrategy fixPointStrategy;
+
+        switch (instr) {
+            case "once":
+                taskName = input.next();
+                taskDef = new TaskDef();
+                taskDef.setName(taskName);
+
+                point = input.nextInt();
+                fixPointStrategy = new FixPointStrategy();
+                fixPointStrategy.setPoint(point);
+
+                OnceLifeCycleStrategy onceLifeCycleStrategy = new OnceLifeCycleStrategy();
+                Task onceTask = new Task();
+                onceTask.setTaskLifeCycleStrategy(onceLifeCycleStrategy);
+                onceTask.setTaskPointCalcStrategy(fixPointStrategy);
+                onceTask.setTaskDef(taskDef);
+
+                taskArrayList.add(onceTask);
+
+                break;
+            case "repeat1":
+                taskName = input.next();
+                taskDef = new TaskDef();
+                taskDef.setName(taskName);
+
+                point = input.nextInt();
+                fixPointStrategy = new FixPointStrategy();
+                fixPointStrategy.setPoint(point);
+
+                count = input.nextInt();
+                TotalCountDownLifeCycleStrategy totalCountDownLifeCycleStrategy = new TotalCountDownLifeCycleStrategy();
+                totalCountDownLifeCycleStrategy.setCount(count);
+
+                Task totalCountTask = new Task();
+                totalCountTask.setTaskLifeCycleStrategy(totalCountDownLifeCycleStrategy);
+                totalCountTask.setTaskPointCalcStrategy(fixPointStrategy);
+                totalCountTask.setTaskDef(taskDef);
+
+                taskArrayList.add(totalCountTask);
+
+                break;
+            case "repeat2":
+                taskName = input.next();
+                taskDef = new TaskDef();
+                taskDef.setName(taskName);
+
+                point = input.nextInt();
+                fixPointStrategy = new FixPointStrategy();
+                fixPointStrategy.setPoint(point);
+
+                count = input.nextInt();
+                DailyCountDownLifeCycleStrategy dailyCountDownLifeCycleStrategy = new DailyCountDownLifeCycleStrategy();
+                dailyCountDownLifeCycleStrategy.setCount(count);
+
+                Task dailyTask = new Task();
+                dailyTask.setTaskLifeCycleStrategy(dailyCountDownLifeCycleStrategy);
+                dailyTask.setTaskPointCalcStrategy(fixPointStrategy);
+                dailyTask.setTaskDef(taskDef);
+
+                taskArrayList.add(dailyTask);
+                break;
+        }
+
+    }
+
+    public static void doTask(String taskname, String username) {
+        TaskPerformer taskPerformer = findTaskPerformer(username);
+        Task task = findTask(taskname);
+
+        try {
+            PerformTaskTransaction performTaskTransaction = new PerformTaskTransaction();
+            boolean success = performTaskTransaction.commit(task, taskPerformer);
+            if (success) {
+                System.out.println("做任务成功！");
+            } else
+                System.out.println("做任务失败！任务已经做过啦！");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    public static void consume(String username,String itemname){
+        TaskPerformer taskPerformer=findTaskPerformer(username);
+        Item item = findItem(itemname);
+        ConsumerTransaction transaction = new ConsumerTransaction();
+        if (transaction.commit(item,taskPerformer)) {
+            System.out.println("您的商品已兑换~");
+        }else {
+            System.out.println("您的商品兑换失败啦");
+        }
+    }
+    public static void newItem(String itemname,int cost){
+        Item fdItem = findItem(itemname);
+        if (fdItem==null) {
+            Item item = new Item(itemname, itemname, cost);
+            items.add(item);
+        }
+        System.out.println("您的商品新建成功啦");
+    }
+    public static void checkFlow(String username){
+        TaskPerformer taskPerformer = findTaskPerformer(username);
+        if (taskPerformer==null){
+            System.out.println("您的用户信息找不到啦");
+            return;
+        }
+
+        List<Flow> flows = taskPerformer.getAccount().getFlows();
+        for (Flow flow:flows){
+            System.out.println(flow);
+        }
+        System.out.println("您的流水线已显示完全啦");
+    }
+    public static void checkAccount(String username){
+        TaskPerformer taskPerformer = findTaskPerformer(username);
+        if (taskPerformer==null){
+            System.out.println("您的用户信息找不到啦");
+            return;
+        }
+        System.out.println(taskPerformer.getAccount().getBalance());
+        System.out.println("您的积分已显示啦");
+    }
+    public static void findCanDoTask(String username){
+        ArrayList<Task> canDoTaskList = new ArrayList<>();
+        canDoTaskList.addAll(taskArrayList);
+        TaskPerformer taskPerformer = findTaskPerformer(username);
+        if (taskPerformer==null){
+            System.out.println("您的用户信息找不到啦");
+            return;
+        }
+        ArrayList<UserTask> userTaskArrayList = new ArrayList<>();
+        try{
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(new File("usertask.txt")));
+            userTaskArrayList = (ArrayList<UserTask>)ois.readObject();
+            ois.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        for (UserTask userTask:userTaskArrayList){
+            if (userTask.getTaskPerformer().equals(taskPerformer) && !userTask.canDo()) {
+                canDoTaskList.remove(userTask.getTask());
+            }
+        }
+        System.out.println("您的待完成任务有：");
+        for (Task task:canDoTaskList){
+            System.out.println(task.getTaskDef().getName());
         }
     }
     private static TaskPerformer findTaskPerformer(String username){
@@ -78,59 +244,6 @@ public class Controller {
         }
         return null;
     }
-    public static void consume(String username,String itemname){
-        TaskPerformer taskPerformer=findTaskPerformer(username);
-        Item item = findItem(itemname);
-        ConsumerTransaction transaction = new ConsumerTransaction();
-        if (transaction.commit(item,taskPerformer)) {
-            System.out.println("您的商品已兑换~");
-        }else {
-            System.out.println("您的商品兑换失败啦");
-        }
-    }
-    public static void newItem(String itemname,int cost){
-        Item fdItem = findItem(itemname);
-        if (fdItem==null) {
-            Item item = new Item(itemname, itemname, cost);
-            items.add(item);
-        }
-        System.out.println("您的商品新建成功啦");
-    }
-    public static void checkFlow(String username){
-        TaskPerformer taskPerformer = findTaskPerformer(username);
-        List<Flow> flows = taskPerformer.getAccount().getFlows();
-        for (Flow flow:flows){
-            System.out.println(flow);
-        }
-        System.out.println("您的流水线已显示完全啦");
-    }
-    public static void checkAccount(String username){
-        TaskPerformer taskPerformer = findTaskPerformer(username);
-        System.out.println(taskPerformer.getAccount().getBalance());
-        System.out.println("您的积分已显示啦");
-    }
-    public static void findCanDoTask(String username){
-        ArrayList<Task> canDoTaskList = new ArrayList<>();
-        for (Task task:taskArrayList){
-            canDoTaskList.add(task);
-        }
-        TaskPerformer taskPerformer = findTaskPerformer(username);
-        ArrayList<UserTask> userTaskArrayList = new ArrayList<>();
-        try{
-            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(new File("usertask.txt")));
-            userTaskArrayList = (ArrayList<UserTask>)ois.readObject();
-            ois.close();
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        for (UserTask userTask:userTaskArrayList){
-            if (userTask.getTaskPerformer().equals(taskPerformer) && !userTask.canDo()) {
-                canDoTaskList.remove(userTask.getTask());
-            }
-        }
-        System.out.println("您的待完成任务有：");
-        for (Task task:canDoTaskList){
-            System.out.println(task.getTaskDef().getName());
-        }
-    }
+
+
 }
