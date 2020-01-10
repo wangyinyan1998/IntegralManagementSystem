ce887d641afe8f1b50cb668a70d5c504bc1e5d87
Yanshiyi <shiyi.yan@sap.com>
2020-01-10 17:14:50 +0800

    完成了！

--- IntegralManagementSystem/src/transaction/Transaction.java
+++ IntegralManagementSystem/src/transaction/Transaction.java
@@ -1,26 +1,8 @@
 package transaction;
 
 import user.TaskPerformer;
-import userTask.Task;
-import userTask.UserTask;
-import userTask.UserTaskAction;
-
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 public abstract class Transaction {
     abstract void addFlow(TaskPerformer taskPerformer,int amount,String desc);
-     void writeObject(String fileName, Object object) {
-        try {
-            File file = new File(fileName);
-            ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(file));
-            output.writeObject(object);
-            output.close();
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 }
